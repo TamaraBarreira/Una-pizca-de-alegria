1d318d8abcf6f8a297df66b7d93e2e4e7b2605d8
Tamara Barreira <tamara.barreira@gmail.com>
2022-07-28 16:07:21 -0300

    agregando funciones y variables al JS de alquileres

--- js/main.js
+++ js/main.js
@@ -16,51 +16,38 @@ class productos{
         this.precio = precio;
         this.alquilado = false;
     }
-    sumandoIva(){
-        this.precio = this.precio * 1.21;
-    }
-}
-
-class listaDePedido{
-    constructor (id,lista,metodopago,descuento){
-        this.id = id;
-        this.lista = lista;
-        this.metodopago = metodopago;
-        this.descuento = descuento; 
-    }
 }
 const productos = [];
-const listaDePedido =[];
 //push//
-productos.push(new Productos (1, "Plaza Montessori", 4000));
-productos.push(new Productos (2, "Juegos de Rol", 4000));
-productos.push(new Productos (3, "Escaladores Pickler", 4000));
-productos.push(new Productos (4, "Carpitas Tipis", 3000));
+productos.push(new Producto(1, "Plaza Montessori", 4000));
+productos.push(new Producto(2, "Juegos de Rol", 4000));
+productos.push(new Producto(3, "Escaladores Pickler", 4000));
+productos.push(new Producto(4, "Carpitas Tipis", 3000));
 
-//for///
-for(const producto of productos){ 
-    producto.sumandoIva()};
-//realizando alquiler 
-const realizarAlquiler = () => {
-    let cantidadArticulo = parseInt (prompt("Seleccione la cantidad de articulos a adquirir"));
-    let listaDeProductos = `Elija los articulos a alquilar seleccionando su numero: \n`;
-    
-}
-const productosElegidos = (cantidadArticulo , productos) => {
-    let compra = [];
-    for (let i = 0; i < cantidadArticulo ; i++){
-        let productoId = parseInt (prompt(listaDeProductosAMostrar + "\n" + productos.map (p => `${p.id}: ${p.nombre} ${p.precio} \n`)));
-        alquiler [i] = productos.find(p=> p.id=== productoId);
-        productos [productoId - 1].alquilado = true;
-    }
-    return alquiler;
+//// variables///
+let item = prompt("Ingrese el nombre del producto a mostrar: \n pLaza montesori \n juegos de rol \n escaladores pickler \n carpitas tipi** Favor de ingresar el nombre en minusculas.** En su defecto ingrese ESC para salir");
 
-let listado = "";
-let alquiler = productosElegidos (cantidadArticulo,productos);
-listaDeAlquiler.push(new ListaAlquileres(Math.round(Math.random() *100000), alquiler, "efectivo",false));
-let total = alquiler.reduce((acc,c)=> acc + c.precio, 0);
-/*    
-alert (`${ listado += "Los productos Alquilados fueron \n" + alquiler.map(m => `${m.nombre}: ${m.precio}\n ) .toSting() .join(" - ") } \n El total a pagar es: ${total}`);
-    return ListaAlquileres;
+//conociendo opciones
+let opc = parseInt(prompt("Ingrese el nombre del producto a mostrar: \n pLaza montesori \n juegos de rol \n escaladores pickler \n carpitas tipi** Favor de ingresar el nombre en minusculas.** En su defecto ingrese ESC para salir"));
+//funciones
+function mostrar(mensaje){
+    alert(mensaje);
 }
-*/
+function descuento (a, b){
+let res = a - b;
+alert (res);
+}
+//algoritmos///////////
+while (item != "ESC"){
+    if (item == "pLaza montesori"){
+        mostrar (Producto.nombre + " " + Producto.precio);
+    } else if (item =="juegos de rol"){
+        mostrar (Producto.nombre + " " + descuento( 4000 , 1000));
+    } else if (item =="escaladores pickler"){
+        mostrar (Producto.nombre + " " + Producto.precio);
+    }else if (item =="carpitas tipi"){
+        mostrar (Producto.nombre + " " + Producto.precio);
+    }else{
+        mostrar("Ingrese un producto de la lista");
+    }item = prompt ("Ingrese el nombre del producto a mostrar: \n pLaza montesori \n juegos de rol \n escaladores pickler \n carpitas tipi** Favor de ingresar el nombre en minusculas.** En su defecto ingrese ESC para salir");
+}
