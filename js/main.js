7f0a5ddd751bf043a1540ed0d40aaf56e4cc5f04
Tamara Barreira <tamara.barreira@gmail.com>
2022-08-01 18:32:46 -0300

    modificando con dom un texto con innerText

--- js/main.js
+++ js/main.js
@@ -51,3 +51,10 @@ while (item != "ESC"){
         mostrar("Ingrese un producto de la lista");
     }item = prompt ("Ingrese el nombre del producto a mostrar: \n pLaza montesori \n juegos de rol \n escaladores pickler \n carpitas tipi** Favor de ingresar el nombre en minusculas.** En su defecto ingrese ESC para salir");
 }
+//////////////DOM///////////////// 
+//modificando con dom un texto
+let titulo = document.getElementById ("textoAlquiler");
+console.log (titulo.innerText);
+/// cambio del contenido del texto
+titulo.innerText = "Alquilate todo!";
+console.log(titulo.innerText);
